b141ded88c669a14e374f24393f8154b40659fe0
xuezhinan <2236199776@qq.com>
2019-05-20 18:35:32 +0800

    增加

--- src/commponts/Add.vue
+++ src/commponts/Add.vue
@@ -3,15 +3,15 @@
     <form class="form-horizontal">
       <div class="form-group">
         <label>用户名</label>
-        <input type="text" class="form-control" placeholder="用户名">
+        <input type="text" class="form-control" placeholder="用户名" v-model="name">
       </div>
       <div class="form-group">
         <label>评论内容</label>
-        <textarea class="form-control" rows="6" placeholder="评论内容"></textarea>
+        <textarea class="form-control" rows="6" placeholder="评论内容" v-model="content"></textarea>
       </div>
       <div class="form-group">
         <div class="col-sm-offset-2 col-sm-10">
-          <button type="button" class="btn btn-default pull-right">提交</button>
+          <button type="button" class="btn btn-default pull-right" @click="sub">提交</button>
         </div>
       </div>
     </form>
@@ -20,7 +20,36 @@
 
 <script>
   export default {
-    name: 'Add'
+    name: 'Add',
+    props:{
+      addComm:{
+        type:Function,
+        required:true
+      }
+    },
+    methods:{
+      sub(){
+        const name = this.name.trim();
+        const content = this.content.trim();
+        if (!name || !content) {
+          alert("名字和内容不能为空！");
+          return
+        }
+        const comment = {
+          name,content
+        }
+        this.addComm(comment);
+        this.name ="";
+        this.content ="";
+
+      }
+    },
+    data(){
+      return {
+        name:"",
+        content:""
+      }
+    }
   }
 </script>
 
