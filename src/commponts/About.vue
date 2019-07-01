11aca852d59bfed36a03bec21776cf94c883a54a
xuezhinan <xuezn@maitewang.com>
2019-07-01 15:06:51 +0800

    添加路由

---
+++ src/commponts/About.vue
@@ -0,0 +1,16 @@
+<template>
+<div>
+  <h2>About组件</h2>
+  <input type="text" class="btn-group">
+</div>
+</template>
+
+<script>
+  export default {
+    name: 'About'
+  }
+</script>
+
+<style scoped>
+
+</style>
